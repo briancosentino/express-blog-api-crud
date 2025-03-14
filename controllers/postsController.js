18e97da79c490663cc2624a685e3d9e5d4a6bc90
briancosentino <cosentinobrian@gmail.com>
2025-03-14 13:57:35 +0100

    add milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,22 +1,29 @@
 /* const express = require(express) */
-
+const posts = require('../data/posts')
 function index (req, res){
-    res.send('get postList')
+    res.json(posts)
 }
 function show (req, res){
-    res.send('show post' + req.params.id)
+    res.json(posts.find(post => post.slug === req.params.id ))
 }
 function store (req, res){
     res.send('create post')
 }
 function update (req, res){
-    res.send('modify partial post' + req.params.id)
+    res.json(posts.find(post => post.id === req.params.id ))
 }
 function modify (req, res){
     res.send('modify whole post' + req.params.id)
 }
 function destroy (req, res){
-    res.send('delete post' + req.params.id)
+    const postIndex =posts.indexOf(posts.find(post => post.slug === req.params.id))
+    posts.splice(postIndex, 1)
+
+    console.log(posts);
+
+    res.sendStatus(204)
+    
+
 }
 
 module.exports = {
